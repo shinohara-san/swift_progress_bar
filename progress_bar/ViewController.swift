9f6c32a310ed973e85a237c019be9c93297057e9
shinohara-san <someiyoshino0809@gmail.com>
2020-10-16 12:24:57 +0900

    Initial Commit

---
+++ progress_bar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  progress_bar
+//
+//  Created by Yuki Shinohara on 2020/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
